4490814a9cd7185c99d17c3d434caa1d0da3eeda
Aleksey Knigin <alexyo2626@gmail.com>
2023-02-09 04:32:31 +0200

    add break & fix bug with undef

--- src/functions/search.js
+++ src/functions/search.js
@@ -1,36 +1,35 @@
 // Search function for the first unique symbol in one word
 function searchFirstUnicSymbolInWord(word) {
-  const ArrSymbols = [];
-
   for (let i = 0; i < word.length; i += 1) {
     let counter = 0;
-    let symbol = word[i];
+    let uniqSymbol = word[i];
     for (let j = 0; j < word.length; j += 1) {
-      if (word[j] === symbol) {
+      if (word[j] === ' ') break;
+      if (word[j] === uniqSymbol) {
         counter += 1;
       }
     }
     if (counter === 1) {
-      ArrSymbols.push(symbol);
+      return uniqSymbol;
     }
   }
-
-  return ArrSymbols[0];
 }
 
 export function searchFerstUnicSymbolInAllText(text) {
   // Сheck for string with spaces
-  if (text.trim() === '') return '';
+  // if (text.trim() === '') return '';
 
   // Delete extra symbols
-  let currentText = text.replaceAll(/[-().,^+!?]/g, '');
+  let currentText = text.replaceAll(/[-().,^+!?"]/g, '');
 
   // Divide the whole text into single words
   currentText = currentText.split(' ');
+  // console.log(currentText);
 
   // Looking for the first unique symbol in each word
   let symbolInEachWord = [];
   for (let i = 0; i < currentText.length; i += 1) {
+    if (currentText[i] === '') continue;
     symbolInEachWord.push(searchFirstUnicSymbolInWord(currentText[i]));
   }
 
